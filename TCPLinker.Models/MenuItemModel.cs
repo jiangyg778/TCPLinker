9110e77b2c4805dba9ba9832e60468c65e9490a5
DESKTOP-DO9B8F8\admin <297138663@qq.com>
2024-12-18 09:17:52 +0800

    Add .gitignore to exclude .vs folder

---
+++ TCPLinker.Models/MenuItemModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zhaoxi.SmartParking.Models
+{
+    public class MenuItemModel
+    {
+        public string Name { get; set; }  // 菜单名称
+        public string IconName { get; set; }  // 图标，可以使用路径或者图片资源
+        public string RegionName { get; set; }  // 对应区域名称
+        public Type ViewType { get; set; }  // 对应视图类型
+    }
+}
